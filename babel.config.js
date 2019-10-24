a63df907de06e6adc73eda9f68f6e2cd9a4dd9ce
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-10-23 20:14:12 -0500

    the final product. to be upgraded later

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+
+const babel = require("@babel/core");
+
+babel.transform("code", optionsObject);
+const presets = [
+    [
+      "@babel/env",
+      {
+        targets: {
+          edge: "17",
+          firefox: "60",
+          chrome: "67",
+          safari: "11.1",
+        },
+        useBuiltIns: "usage",
+      },
+    ],
+  ];
+  
+  module.exports = { presets };
